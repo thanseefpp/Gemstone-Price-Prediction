2649665a818aac1052f680186fcb06abd6ca16e8
thanseefpp <androtsf@gmail.com>
2023-07-28 17:03:30 +0400

    setup completed

---
+++ setup.py
@@ -0,0 +1,53 @@
+import os
+from setuptools import find_packages, setup
+
+REPO_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def getVersion():
+    """
+    Get version from local file.
+    """
+    with open(os.path.join(REPO_DIR, "VERSION"), "r") as versionFile:
+        return versionFile.read().strip()
+
+
+def parse_file(requirementFile):
+    try:
+        return [
+            line.strip()
+            for line in open(requirementFile).readlines()
+            if not line.startswith("-")
+        ]
+    except IOError:
+        return []
+
+
+def findRequirements():
+    """
+    Read the requirements.txt file and parse into requirements for setup's
+    install_requirements option.
+    """
+    requirementsPath = os.path.join(REPO_DIR, "requirements.txt")
+    return parse_file(requirementsPath)
+
+
+if __name__ == "__main__":
+    requirements = findRequirements()
+    REPO_NAME = "Gemstone-Price-Prediction"
+    AUTHOR_USER_NAME = "thanseefpp"
+    SRC_REPO = "Gemstone"
+    setup(
+        name=SRC_REPO,
+        version=getVersion(),
+        description='Gemstone Price Prediction, Resources Collected From Kaggle',
+        author=AUTHOR_USER_NAME,
+        author_email='thanseefpp@gmail.com',
+        install_requires=requirements,
+        url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+        project_urls={
+            "Bug tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        },
+        package_dir={"":"src"},
+        packages=find_packages(where="src"),
+    )
