55d20329350100713b6291a3a3d84cf21b318b91
thanseefpp <androtsf@gmail.com>
2023-08-01 14:54:47 +0400

    data ingestion functions done

--- src/Gemstone/components/data_ingestion.py
+++ src/Gemstone/components/data_ingestion.py
@@ -3,38 +3,46 @@ import os
 import sys
 import pandas as pd
 from dataclasses import dataclass
-from src.Gemstone.config.logger import logging
-from src.Gemstone.config.exception import CustomException
-from zenml.steps import step
-
+from src.exception import CustomException
+from src.logger import logging
 #----------------------------------------- FUNCTIONS/CLASSES ------------------------------------------#
+
+@dataclass
+class DataIngestionConfig:
+    """
+        Data ingestion Config class which return three file paths.
+    """
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 class IngestData:
     """
-    Data ingestion class which ingests data from the source and returns a DataFrame.
+        Data ingestion class which ingests data from the source and returns a DataFrame.
     """
 
-    def __init__(self) -> None:
+    def __init__(self):
         """Initialize the data ingestion class."""
-        pass
+        self.ingestion_config = DataIngestionConfig()
 
     def get_data(self) -> pd.DataFrame:
         try:
             logging.info("Collecting Dataset")
-            return pd.read_csv("../../data/gemstone_price.csv")
+            return pd.read_csv("data/gemstone_price.csv")
         except Exception as e:
             raise CustomException(e,sys) from e
         
-@step
-def ingest_data() -> pd.DataFrame:
-    """
-    Args:
-        None
-    Returns:
-        df: pd.DataFrame
-    """
-    try:
-        ingest_data = IngestData()
-        return ingest_data.get_data()
-    except Exception as e:
-        raise CustomException(e,sys) from e
+    def initiate_ingest_data(self)-> pd.DataFrame:
+        """
+        Args:
+            None
+        Returns:
+            df: pd.DataFrame
+        """
+        try:
+            df = self.get_data()
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False) # saving the data
+            return df
+        except Exception as e:
+            raise CustomException(e,sys) from e
