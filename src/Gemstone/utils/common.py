4bdcee0752ca4f70292e0d64e948a745344fd051
thanseefpp <androtsf@gmail.com>
2023-08-01 13:20:42 +0400

    model save and load functions added to utils

--- src/Gemstone/utils/common.py
+++ src/Gemstone/utils/common.py
@@ -1,19 +1,37 @@
+#----------------------------------------- IMPORTING DEPENDENCIES -------------------------------------#
+import os
 import sys
+import pickle
+import numpy as np 
+import pandas as pd
+from src.Gemstone.config.exception import CustomException
+from src.Gemstone.config.logger import logger
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+#----------------------------------------- FUNCTIONS/CLASSES -----------------------------------------#
+
+def load_object(file_path):
+    """
+        Loading the object from a specific path
+    """
+    try:
+        with open(file_path, 'rb') as file:
+            return pickle.load(file)
+    except Exception as e:
+        raise CustomException(e, sys) from e
 
 
-def error_message_detail(error, error_details: sys):
-    _, _, exc_tb = error_details.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
+def save_object(file_path, obj):
+    """
+        saving the object in a specific path
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logger.info(f"Object Saved at : {file_path}")
+    except Exception as e:
+        raise CustomException(e, sys) from e
+    
 
-
-class CustomException(Exception):
-    def __init__(self, error_message, error_details: sys):
-        super().__init__(error_message)  # inheriting init
-        self.error_message = error_message_detail(
-            error=error_message, error_details=error_details)
-
-    def __str__(self) -> str:
-        return self.error_message
